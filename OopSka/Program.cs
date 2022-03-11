4ecce05051ac581cf563ab76f99b06b3f50c8447
jay hsieh <jay.hsieh@remotes.com.tw>
2022-03-11 15:47:54 +0800

    Initial commit

---
+++ OopSka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OopSka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
